2567668bfd8b58fb46e68cb4d7c10f24da5baaf7
Kosei Masuda <masudakousei@mk.local>
2024-04-27 21:25:53 +0800

    edited code

--- main.go
+++ main.go
@@ -1,13 +1,45 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"gin/controllers"
+	"gin/models"
+	"gin/repositories"
+	"gin/services"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	items := []models.Item{
+		{
+			ID:          1,
+			Name:        "商品1",
+			Price:       1000,
+			Description: "説明1",
+			Soldout:     false,
+		},
+		{
+			ID:          2,
+			Name:        "商品2",
+			Price:       1000,
+			Description: "説明2",
+			Soldout:     true,
+		},
+		{
+			ID:          3,
+			Name:        "商品3",
+			Price:       1000,
+			Description: "説明3",
+			Soldout:     false,
+		},
+	}
+
+	itemRepository := repositories.NewItemMemoryRepository(items)
+	itemService := services.NewItemService(itemRepository)
+	itemController := controllers.NewItemContoroller(itemService)
+
 	r := gin.Default()
-	r.GET("/example", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.GET("/items", itemController.FindAll)
+	r.GET("/items/:id", itemController.FindById)
 	r.Run("localhost:8080") // listen and serve on 0.0.0.0:8080
 }
